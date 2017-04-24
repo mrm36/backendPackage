223a5b919663c0cdfb33b889261236f245fcbec4
Matthew MIller <mrm36@njit.edu>
2017-04-24 15:28:14 -0400

    Removed rabbitmq config from the package script

--- package_backend.sh
+++ package_backend.sh
@@ -1,26 +1,24 @@
 echo "Packaging Bundle..."
 	cd /home/matt/Scripts/temp/
+	echo "MySQL Password?"
 		mkdir mySqlData
 			cd mySqlData/
 				mysqldump -u root -p login > backup_sql.sql
 				cd ..
 	
-	  beScrDIR=/home/matt/git/it490_backend_final/it490_backend_final
-	  rabbitMQConf=/var/lib/rabbitmq
-	
+	beScrDIR=/home/matt/git/it490_backend_final/it490_backend_final
 		cp -a $beScrDIR* /home/matt/Scripts/temp/
 
-		mkdir rabbitMQData
-			cp -a $rabbitMQConf* /home/matt/Scripts/temp/rabbitMQData/ 
-	
 	tar -czvf backendPackage.tar.gz -C /home/matt/Scripts/temp/ . 
-	rm -r /home/matt/Scripts/temp/it490_backend_final/
-	rm -r /home/matt/Scripts/temp/mySqlData/
-	rm -r /home/matt/Scripts/temp/rabbitMQData/
+		rm -r /home/matt/Scripts/temp/it490_backend_final/
+		rm -r /home/matt/Scripts/temp/mySqlData/
         echo `ls | grep backendBundle`
-	echo "Bundle Complete"
-
+		echo "Bundle Complete"
+	
+	# Scp package to the deploy server
 	echo "Sending to deploy server..."
 	scp -r /home/matt/Scripts/temp/backendPackage.tar.gz uzair@192.168.2.30:/home/uzair/Packages/backend
+	
+	cp backendPackage.tar.gz ../backendPackage/
 	rm /home/matt/Scripts/temp/backendPackage.tar.gz
 	php /home/matt/git/rabbitmqphp_example_deployTest/testRabbitMQClient.php deployBroadcast backendQA
